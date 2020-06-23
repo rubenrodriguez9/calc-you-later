e717dc5ab18679f805d273ec5205594696a77c8f
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-06-23 14:24:21 -0400

    completed without stretch goals

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,9 +1,13 @@
 /*********************************************
  * YOUR CODE HERE!                           *
  ********************************************/
+const calculate = require("./calculate-back-end")
 
+let num1 = getInput(0);
+let sign = getInput(1);
+let num2 = getInput(2);
 
-
+console.log(calculate(num1, num2, sign))
 
 
 
